465e0ba1bf2ba975d6540facea6efe93d15cf35a
ManuelR76 <romeromanuel76@gmail.com>
2018-10-09 13:39:27 +0200

    exo 2

---
+++ assets/JS/script.js
@@ -0,0 +1,4 @@
+var lastName = "Doe"; //Déclaration de 3 variables//
+var firstName = "John";
+var city = "New-York";
+alert("Nom : " + lastName  + "\nPrénom : " + firstName + "\nVille : " + city); //Affichage de ces 3 variables//
